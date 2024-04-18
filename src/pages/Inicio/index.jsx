59c0e1d3a401532a6d3dcd7efff4e3ee2e39ecab
henrithanN <jonathanhenrique1515@gmail.com>
2024-04-18 16:39:56 -0300

    <feat> Criando componente de Titulo

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,12 +1,16 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
+import Titulo from "components/Titulo";
 
 const Inicio = () => {
     return (
         <>
             <Cabecalho />
             <Banner imagem="home" />
+            <Titulo>
+                <h1>Um Lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
             <Rodape />
         </>
     )
