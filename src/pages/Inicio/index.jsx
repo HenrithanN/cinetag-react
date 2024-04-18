1e80c174f95b2d96f4dcbe332dc5ad1d975176de
henrithanN <jonathanhenrique1515@gmail.com>
2024-04-18 16:52:25 -0300

    <feat> Criando componente de card

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,5 +1,6 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 
@@ -11,6 +12,7 @@ const Inicio = () => {
             <Titulo>
                 <h1>Um Lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
+            <Card id={1} titulo='Teste' capa='https://github.com/henrithanN.png'></Card>
             <Rodape />
         </>
     )
