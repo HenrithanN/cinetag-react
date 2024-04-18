f8c2fa5e522427fae1dfe8166eab5dcd0c7f4e82
henrithanN <jonathanhenrique1515@gmail.com>
2024-04-18 16:31:11 -0300

    <feat> Ajuste adicionando link pra github no rodapé

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -3,7 +3,11 @@ import styles from './Rodape.module.css';
 const Rodape = () => {
     return (
         <footer className={styles.rodape}>
-            <h2>Desenvolvido por HenrithanN</h2>
+            <h2>Desenvolvido por: 
+                <a href="https://github.com/henrithanN" rel='noreferrer' target='_blank'>
+                    @HenrithanN
+                </a>    
+            </h2>
         </footer>
     )
 }
