6d363470f5172cfffb1ca073cd865af4e321b900
henrithanN <jonathanhenrique1515@gmail.com>
2024-04-18 15:40:44 -0300

    <feat> Criando componente de página de início

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import Inicio from 'pages/Inicio';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const AppRoutes = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Inicio />} ></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes;
