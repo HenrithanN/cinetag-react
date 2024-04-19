6216f22d33ab8fd266a1da0b091bf56004b5ccbf
henrithanN <jonathanhenrique1515@gmail.com>
2024-04-19 11:15:01 -0300

    <feat> adicionando lista de favoritos na página de favoritos de forma dinâmica

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -2,15 +2,25 @@ import styles from './Favoritos.module.css';
 import Banner from 'components/Banner';
 import Card from 'components/Card';
 import Titulo from 'components/Titulo';
+import { useFavoritoContext } from 'contextos/Favoritos';
 
 const Favoritos = () => {
+
+    const { favorito } = useFavoritoContext();
+
     return (
         <>
             <Banner imagem='favoritos' />
             <Titulo>
                 <h1>Meus Favoritos</h1>
             </Titulo>
-            <Card titulo="teste" capa="https://github.com/henrithanN.png"/>        
+            <section className={styles.container}>
+                {
+                    favorito.map((item) => {
+                        return <Card {...item} id={item.id}/>
+                    })
+                }
+            </section>
         </>
     )
 }
