cf1bd92e7e93ddaccfc88d51495974559b8e189a
henrithanN <jonathanhenrique1515@gmail.com>
2024-04-19 09:35:38 -0300

    <feat> Criando componente de container para manter os arquivos centralizados no arquivo routes

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,19 @@ import Inicio from 'pages/Inicio';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cabecalho from './components/Cabecalho'
 import Rodape from './components/Rodape'
+import Container from 'components/Container';
+
 const AppRoutes = () => {
     return(
         <>
         <BrowserRouter>
             <Cabecalho />
-                <Routes>
-                    <Route path="/" element={<Inicio />} ></Route>
-                    <Route path="/Favoritos" element={<Favoritos />} ></Route>
-                </Routes>
+                <Container>
+                    <Routes>
+                        <Route path="/" element={<Inicio />} ></Route>
+                        <Route path="/Favoritos" element={<Favoritos />} ></Route>
+                    </Routes>
+                </Container>
             <Rodape />
         </BrowserRouter>
         </>
