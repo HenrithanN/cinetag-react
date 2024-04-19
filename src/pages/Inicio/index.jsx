c0f6a70247370f8613c79360842d53e17b0b556a
henrithanN <jonathanhenrique1515@gmail.com>
2024-04-19 08:55:30 -0300

    <feat> Consumindo lista de filmes do arquivo JSON

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,18 +1,25 @@
-import Banner from "components/Banner";
-import Cabecalho from "components/Cabecalho";
-import Card from "components/Card";
-import Rodape from "components/Rodape";
-import Titulo from "components/Titulo";
-
+import Banner from 'components/Banner';
+import Cabecalho from 'components/Cabecalho';
+import Card from 'components/Card';
+import Rodape from 'components/Rodape';
+import Titulo from 'components/Titulo';
+import styles from './Inicio.module.css';
+import listaFilmes from 'json/db.json'
 const Inicio = () => {
     return (
         <>
             <Cabecalho />
-            <Banner imagem="home" />
+            <Banner imagem='home' />
             <Titulo>
                 <h1>Um Lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
-            <Card id={1} titulo='Teste' capa='https://github.com/henrithanN.png'></Card>
+            <section className={styles.container}>
+                {
+                    listaFilmes.map((filme) => {
+                        return <Card {...filme} key={filme.id} />
+                    })
+                }
+            </section>
             <Rodape />
         </>
     )
