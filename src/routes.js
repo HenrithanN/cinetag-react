9602a383d89c6b626ff95492681670c697391908
henrithanN <jonathanhenrique1515@gmail.com>
2024-04-19 11:51:46 -0300

    <feat> criando componente de Página base, e consumindo rotas aninhadas

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,22 @@
 import Favoritos from 'pages/Favoritos';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Cabecalho from './components/Cabecalho'
-import Rodape from './components/Rodape'
-import Container from 'components/Container';
-import FavoritosProvider from 'contextos/Favoritos';
 import Player from 'pages/Player';
 import NaoEncontrada from 'pages/NaoEncontrado';
+import PaginaBase from 'pages/PaginaBase';
 
 const AppRoutes = () => {
     return(
         <>
         <BrowserRouter>
-            <Cabecalho />
-                <Container>
-                    <FavoritosProvider>
-                        <Routes>
-                            <Route path="/" element={<Inicio />} ></Route>
-                            <Route path="/Favoritos" element={<Favoritos />} ></Route>
-                            <Route path="/Player/:id" element={<Player />} ></Route>
-                            <Route path="*" element={<NaoEncontrada />} ></Route>
-                        </Routes>
-                    </FavoritosProvider>
-                </Container>
-            <Rodape />
+            <Routes>
+                <Route path="/" element={<PaginaBase />} >
+                    <Route index element={<Inicio />} ></Route>
+                    <Route path="Favoritos" element={<Favoritos />} ></Route>
+                    <Route path="Player/:id" element={<Player />} ></Route>
+                    <Route path="*" element={<NaoEncontrada />} ></Route>
+                </Route>
+            </Routes>
         </BrowserRouter>
         </>
     )
