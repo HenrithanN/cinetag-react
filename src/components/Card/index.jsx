f3eaaa0172ae158de0293e8edd68382eed69ad88
henrithanN <jonathanhenrique1515@gmail.com>
2024-04-19 10:01:36 -0300

    <feat> Criando card fixo para página de favoritos

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,7 +3,7 @@ import iconeFavoritar from './favoritar.png';
 
 const Card = ({ id, titulo, capa }) => {
     return (
-        <div className={styles.container}>
+        <div className={styles.container} id={id}>
             <img src={capa} alt={titulo} className={styles.capa} />
             <h2>{ titulo }</h2>
             <img src={iconeFavoritar} alt='Icone favoritar filme' className={styles.favoritar} />
--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,9 +1,16 @@
 import styles from './Favoritos.module.css';
+import Banner from 'components/Banner';
+import Card from 'components/Card';
+import Titulo from 'components/Titulo';
 
 const Favoritos = () => {
     return (
         <>
-            <h1>Favoritos</h1>
+            <Banner imagem='favoritos' />
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+            <Card titulo="teste" capa="https://github.com/henrithanN.png"/>        
         </>
     )
 }
